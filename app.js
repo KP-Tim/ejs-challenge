07860f89eb035f10cce1dbdbbb8daae7488d6abb
KP-Tim <kancanha@gmail.com>
2022-06-24 09:44:11 -0700

    challenge 8, console.log compose input

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
+app.post("/compose", function (req, res) {
+  let comp = req.body.postTitle;
+  console.log(comp);
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
